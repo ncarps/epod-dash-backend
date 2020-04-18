cfbc2244bb0126f24420f722c42a91e8b5a84bb3
christiandendulay <christian.den.dulay@gmail.com>
2020-04-18 11:36:19 +0800

    Fix wrong calculation in variance report

--- src/helper.ts
+++ src/helper.ts
@@ -39,16 +39,25 @@ export const varianceReportShipment = (
       .map((del: any) => {
         let variance = 0;
         del.items.map((item: any) => {
-          const convertVariance = (variance: number) => {
+          const convertVariance = (variance: number, qty: number) => {
+            console.log(variance);
+
             if (variance < 0) {
               return variance * -1;
             }
+            if (variance > 0) {
+              return variance * -1;
+            }
+
             return variance;
           };
           const qty = item.qty;
-          const varianceQty = convertVariance(item.varianceQty) || 0;
-          variance = variance + (varianceQty / qty) * 100;
+          const varianceQty = convertVariance(item.varianceQty, item.qty) || 0;
+          const newVariance = (varianceQty / qty) * 100;
+
+          variance = variance + newVariance;
         });
+        console.log(del.items.length);
         variance = variance != 0 ? variance / del.items.length : 0;
         return {
           delivery: del.id,
