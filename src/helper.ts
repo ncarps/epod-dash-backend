196b7f4e8bb32036204a89f8c6357a59c5155a7d
christiandendulay <christian.den.dulay@gmail.com>
2020-04-18 11:05:39 +0800

    Fix variance report per shipment

--- src/helper.ts
+++ src/helper.ts
@@ -35,27 +35,75 @@ export const varianceReportShipment = (
   return shipmentNumber.map((shipment) => {
     return deliveries
       .filter((del: any) => del.shipmentNumber === shipment)
+      .filter((del: any) => del.delvStatus === "Complete")
       .map((del: any) => {
-        if (del.delvStatus === "Complete") {
-          let variance = 0;
-          del.items.map((item: any) => {
-            const convertVariance = (variance: number) => {
-              if (variance < 0) {
-                return variance * -1;
-              }
-              return variance;
-            };
-            const qty = item.qty;
-            const varianceQty = convertVariance(item.varianceQty) || 0;
-            variance = variance + (varianceQty / qty) * 100;
-          });
-          variance = variance != 0 ? variance / del.items.length : 0;
-          return {
-            delivery: del.id,
-            variance: variance,
-            shipment: shipment,
+        let variance = 0;
+        del.items.map((item: any) => {
+          const convertVariance = (variance: number) => {
+            if (variance < 0) {
+              return variance * -1;
+            }
+            return variance;
           };
-        }
+          const qty = item.qty;
+          const varianceQty = convertVariance(item.varianceQty) || 0;
+          variance = variance + (varianceQty / qty) * 100;
+        });
+        variance = variance != 0 ? variance / del.items.length : 0;
+        return {
+          delivery: del.id,
+          variance: variance,
+          shipment: shipment,
+        };
       });
+  });
+};
+
+export const fetchDelivery = async () => {
+  return await fetchEpodServer({
+    query: `query{
+            allDeliverys{
+              id
+              scheduledDate
+              scheduledTime
+              delvStatus
+              driver{
+                id
+                name
+                plateNumber
+              }
+              items{
+                id
+                itemNumber
+                material
+                pricePerUnit
+                uom
+                qty
+                varianceQty
+                pricePerUnit
+              }
+              customer{
+                id
+                name
+                address{
+                  id
+                  building_name
+                  street
+                  city
+                  state
+                  street
+                  zip_code
+                }
+              }
+              shipmentNumber
+              file{
+                 id
+                path
+              }
+            }
+          }`,
+    variables: {},
+  }).then((res: any) => {
+    return res.data;
   });
 };
