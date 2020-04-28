979fac80a38c8e54ac3895c7a287fbb48b705a17
christiandendulay <christian.den.dulay@gmail.com>
2020-04-28 16:45:26 +0800

    change to apollo-link

--- src/helper.ts
+++ src/helper.ts
@@ -1,4 +1,7 @@
 const { createApolloFetch } = require("apollo-fetch");
+import { execute, makePromise } from "apollo-link";
+import { HttpLink } from "apollo-link-http";
+import gql from "graphql-tag";
 
 export const fetchEpodServer = createApolloFetch({
   uri: "http://localhost:4000/graphql",
@@ -181,52 +184,115 @@ export const varianceReportShipment = (
 };
 
 export const fetchDelivery = async () => {
-  return await fetchEpodServer({
-    query: `query{
-            allDeliverys{
+  // return await fetchEpodServer({
+  //   query: `query{
+  //           allDeliverys{
+  //             id
+  //             scheduledDate
+  //             scheduledTime
+  //             delvStatus
+  //             driver{
+  //               id
+  //               name
+  //               plateNumber
+  //             }
+  //             items{
+  //               id
+  //               itemNumber
+  //               material
+  //               pricePerUnit
+  //               uom
+  //               qty
+  //               varianceQty
+  //               pricePerUnit
+  //               deliveryDateAndTime
+  //             }
+  //             customer{
+  //               id
+  //               name
+  //               address{
+  //                 id
+  //                 building_name
+  //                 street
+  //                 city
+  //                 state
+  //                 street
+  //                 zip_code
+  //               }
+  //             }
+  //             shipmentNumber
+  //             file{
+  //                id
+  //               path
+  //             }
+  //             trucker
+  //           }
+  //         }`,
+  //   variables: {},
+  // }).then((res: any) => {
+  //   return res.data;
+  // });
+  const uri = "http://localhost:4000/graphql";
+  const link = new HttpLink({ uri });
+  const operation = {
+    query: gql`
+      query {
+        allDeliverys {
+          id
+          scheduledDate
+          scheduledTime
+          delvStatus
+          driver {
+            id
+            name
+            plateNumber
+          }
+          items {
+            id
+            itemNumber
+            material
+            pricePerUnit
+            uom
+            qty
+            varianceQty
+            pricePerUnit
+            deliveryDateAndTime
+          }
+          customer {
+            id
+            name
+            address {
               id
-              scheduledDate
-              scheduledTime
-              delvStatus
-              driver{
-                id
-                name
-                plateNumber
-              }
-              items{
-                id
-                itemNumber
-                material
-                pricePerUnit
-                uom
-                qty
-                varianceQty
-                pricePerUnit
-                deliveryDateAndTime
-              }
-              customer{
-                id
-                name
-                address{
-                  id
-                  building_name
-                  street
-                  city
-                  state
-                  street
-                  zip_code
-                }
-              }
-              shipmentNumber
-              file{
-                 id
-                path
-              }
-              trucker
+              building_name
+              street
+              city
+              state
+              street
+              zip_code
             }
-          }`,
+          }
+          shipmentNumber
+          file {
+            id
+            path
+          }
+          trucker
+        }
+      }
+    `,
     variables: {},
-  }).then((res: any) => {
-    return res.data;
-  });
+    context: {
+      headers: {
+        //Den auth
+        //Authorization: "Basic cXdlcnR5OmFhYQ==",
+        //Mark auth
+        Authorization: "Basic bWFyazoxMjM=",
+      },
+    },
+  };
+
+  const result: any = await makePromise(execute(link, operation))
+    .then((data) => data)
+    .catch((error) => error);
+  return result.data || result.error;
 };
